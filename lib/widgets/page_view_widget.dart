e46b75164008bdc449cf5d4899baa8f68752a5c1
Benedict Contawe <benedictcontawe@gmail.com>
2024-03-07 14:51:13 +0800

    add PageViewWidget

---
+++ lib/widgets/page_view_widget.dart
@@ -0,0 +1,35 @@
+import 'package:app/widgets/base_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class PageViewWidget extends BaseWidget {
+  
+  const PageViewWidget ( {
+    super.key,
+    this.canScroll,
+    required this.pageController,
+    required this.isLoading,
+    required this.widgets,
+  } );
+
+  final bool? canScroll;
+  final PageController pageController;
+  final RxBool isLoading;
+  final List<Widget> widgets;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Obx ( () {
+      if (isLoading.isTrue) {
+        return const Center(child: CircularProgressIndicator());
+      } else {
+        return PageView (
+          controller: pageController,
+          physics: canScroll == true  ? const ClampingScrollPhysics() : const NeverScrollableScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          children: widgets,
+        );
+      }
+    }, );
+  }
+}
