d90ecda752381b3a445d97fd8cd88db34f2ba198
Benedict Contawe <benedictcontawe@gmail.com>
2024-03-10 13:05:21 +0800

    update dashboard

--- lib/controllers/dashboard_controller.dart
+++ lib/controllers/dashboard_controller.dart
@@ -1,5 +1,6 @@
 import 'package:app/controllers/base_controller.dart';
 import 'package:flutter/foundation.dart';
+import 'package:get/get.dart';
 
 class DashboardController extends BaseController {
 
@@ -7,10 +8,37 @@ class DashboardController extends BaseController {
     debugPrint("DashboardController Constructor");
   }
 
+  List<String> _topComplaintsList = [];
+
   @override
   Future<void> onInit() async {
     super.onInit();
     debugPrint("DashboardController onInit");
+    _topComplaintsList = ["Sample First", "Sample Second", "Sample Third"]; //TODO: Get Data from Database
+  }
+
+  int getTopComplaintsCount() {
+    return _topComplaintsList.length;
+  }
+
+  String getTopComplaints(int index) {
+    return _topComplaintsList[index];
+  }
+
+  List<String> getTopComplaintsList() {
+    return _topComplaintsList;
+  }
+
+  String getSolvedComplaints() {
+    return "Sample for Solved Complaints"; //TODO: Get Data from Database
+  }
+
+  String getPendingComplaints() {
+    return "Sample for Pending Complaints"; //TODO: Get Data from Database
+  }
+
+  String getComplaintsSubmittedToday() {
+    return "Sample for Submitted Today"; //TODO: Get Data from Database
   }
   
   @override
--- lib/views/dashboard_page.dart
+++ lib/views/dashboard_page.dart
@@ -1,6 +1,8 @@
 import 'package:app/controllers/dashboard_controller.dart';
+import 'package:app/utils/app_localizations.dart';
 import 'package:app/views/base_view.dart';
 import 'package:app/widgets/dashboard_app_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DashboardPage extends BaseView<DashboardController> {
@@ -12,9 +14,85 @@ class DashboardPage extends BaseView<DashboardController> {
     return Scaffold (
       appBar: DashboardAppBarWidget(height: MediaQuery.of(context).size.height * 0.20, widthGap: MediaQuery.of(context).size.width * 0.05,),
       backgroundColor: Colors.grey,
-      body: Container (
-        color: Colors.grey,
-        child: const Center( child: Text("DashboardPage")) ,
+      body: Row (
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget> [
+          Card (
+            child: Stack (
+              alignment: Alignment.center,
+              children: <Widget> [
+                const Positioned(top: 10.0, right: 10.0, child: Icon(CupertinoIcons.pencil,),),
+                Column (
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget> [
+                    const Divider(),
+                    Text( AppLocalizations.of(context).translate('top_complaints_in_barangay_bonbon') ),
+                    const Divider(),
+                    Column( children: controller.getTopComplaintsList().map((topComplaint) => Text(topComplaint)).toList(),)
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Column (
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget> [
+              Card (
+                child: Stack (
+                  children: <Widget> [
+                    const Positioned(top: 10.0, right: 10.0, child: Icon(CupertinoIcons.pencil,),),
+                    Column ( 
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center, 
+                      children: <Widget> [
+                        const Divider(),
+                        Text(AppLocalizations.of(context).translate('solved_complaints')),
+                        SizedBox( width: MediaQuery.of(context).size.width * 0.3, child: const Divider( color: Colors.grey, thickness: 1.0),),
+                        Text( controller.getSolvedComplaints() ),
+                      ],
+                    ),
+                  ],
+                )
+              ),
+              Card (
+                child: Stack (
+                  children: <Widget> [
+                    const Positioned(top: 10.0, right: 10.0, child: Icon(CupertinoIcons.pencil,),),
+                    Column ( 
+                      children: <Widget> [ 
+                        const Divider(),
+                        Text(AppLocalizations.of(context).translate('pending_complaints')),
+                        SizedBox( width: MediaQuery.of(context).size.width * 0.3, child: const Divider( color: Colors.grey, thickness: 1.0),),
+                        Text( controller.getPendingComplaints() ),
+                      ],
+                    ),
+                  ],
+                )
+              ),
+              Card (
+                child: Stack (
+                  children: <Widget> [
+                    const Positioned(top: 10.0, right: 10.0, child: Icon(CupertinoIcons.pencil,),),
+                    Column ( 
+                      children: <Widget> [
+                        const Divider(),
+                        Text(AppLocalizations.of(context).translate('complaints_submitted_today')), 
+                        SizedBox( width: MediaQuery.of(context).size.width * 0.3, child: const Divider( color: Colors.grey, thickness: 1.0),),
+                        Text( controller.getComplaintsSubmittedToday() ),
+                      ],
+                    ),
+                  ],
+                )
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
